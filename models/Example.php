37f4ed5568c1a4f50575142a1e6fa68be4b2fc5d
costi <cnecula20@yahoo.ro>
2020-03-25 05:50:10 +0200

    make default model

--- models/Example.php
+++ models/Example.php
@@ -4,7 +4,7 @@ namespace models;
 use core\Model;
 
 /**
- * protected $primaryKey = 'ID';
+ * const PRIMARYKEY = 'ID';
  * const TABLE = 'table_name';(default is strtolower(model_name).'s' ex Example => examples
  */
 
